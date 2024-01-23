cacd5e5a8ad1190eb1c2e530a6fb42a79ca196ef
Geovanny <birdcode.py@gmail.com>
2024-01-23 18:28:27 -0500

    Segmentación geográfica de registros de colibries por Municipio

--- ProyectoBirdCodeCundinamarca.sql
+++ ProyectoBirdCodeCundinamarca.sql
@@ -9,3 +9,17 @@ SELECT species, COUNT(DISTINCT id) AS rec_gbif
 FROM trochilidae_cundinamarca
 GROUP BY species ORDER BY rec_gbif DESC;
 
+-- Creación de campo de municipios en la base de datos de colibries de Cundinamarca
+
+ALTER TABLE trochilidae_cundinamarca ADD COLUMN municipio varchar(90);
+
+-- Segmentación geográfica de registros de colibries de cundinamarca por Municipio
+
+UPDATE trochilidae_cundinamarca SET municipio = municipios_cundinamarca.mpio_cnmbr
+FROM municipios_cundinamarca 
+WHERE ST_Intersects(trochilidae_cundinamarca.geom, municipios_cundinamarca.geom);
+
+UPDATE trochilidae_cundinamarca SET municipio = bogota.mpio_cnmbr
+FROM bogota
+WHERE ST_Intersects(trochilidae_cundinamarca.geom, bogota.geom);
+
