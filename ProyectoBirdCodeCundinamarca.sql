9cb81683221be6f041517767bb204e073b212afc
Geovanny <birdcode.py@gmail.com>
2024-01-23 17:20:42 -0500

    Diagnostico preliminar de especies de colibries en Cundinamarca

--- ProyectoBirdCodeCundinamarca.sql
+++ ProyectoBirdCodeCundinamarca.sql
@@ -1,1 +1,11 @@
+-- Creación de extensión PostGIS para soporte de datos espaciales
+
 CREATE EXTENSION postgis;
+
+-- Consulta de especies de colibries en el territorio del departamento de Cundinamarca
+-- y número de registros en la base de datos de la GBIF para cada especie presente
+
+SELECT species, COUNT(DISTINCT id) AS rec_gbif
+FROM trochilidae_cundinamarca
+GROUP BY species ORDER BY rec_gbif DESC;
+
